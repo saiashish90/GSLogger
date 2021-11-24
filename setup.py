dcd314683d34e3c7a836f4d23ee7aa86b03cdaf5
saiashish90 <33771241+saiashish90@users.noreply.github.com>
2021-11-24 16:29:20 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="GLogger",
-    packages=find_packages(include=["lib"]),
+    packages=find_packages(include=["Glogger"]),
     version="0.1.0",
     description="Grafana and slack logger library",
     author="Sai Ashish",
