4e3839ce3ea22a3b188b9d1aa24f0e38cfdf0a52
Sai Ashish <sai.ashish@datapoem.com>
2021-12-26 10:44:46 +0000

    changed the logger to have stored tags

--- Glogger/loki.py
+++ Glogger/loki.py
@@ -4,7 +4,7 @@ curr_datetime = time.time_ns()
 
 # initialize the loki client class
 class Loki:
-    def __init__(self, url=None):
+    def __init__(self, url=None, **tags):
         if url:
             self.url = url
         else:
@@ -13,11 +13,15 @@ class Loki:
             except KeyError:
                 print("Please set the LOKI_URL environment variable")
                 exit(1)
+        # create a dict from the kwargs
+        self.tags = tags
 
     # function to post logs to loki
     def info(self, msg, **tags):
         # Constructing the payload
         curr_datetime = time.time_ns()
+        # append the class tags to the tags
+        tags.update(self.tags)
         payload = json.dumps(
             {"streams": [{"stream": tags, "values": [[curr_datetime, f"[INFO] {msg}"]]}]}
         )
@@ -32,6 +36,8 @@ class Loki:
     def warning(self, msg, **tags):
         # Constructing the payload
         curr_datetime = time.time_ns()
+        # append the class tags to the tags
+        tags.update(self.tags)
         payload = json.dumps(
             {"streams": [{"stream": tags, "values": [[curr_datetime, f"[WARNING] {msg}"]]}]}
         )
@@ -46,6 +52,8 @@ class Loki:
     def error(self, msg, **tags):
         # Constructing the payload
         curr_datetime = time.time_ns()
+        # append the class tags to the tags
+        tags.update(self.tags)
         payload = json.dumps(
             {
                 "streams": [
