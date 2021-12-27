f93b6590b07a37076d18b88888f9c84b371b9a01
Sai Ashish <33771241+saiashish90@users.noreply.github.com>
2021-12-27 17:52:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ import os
 load_dotenv()
 
 logger = Loki(
-    url="https://129662:eyJrIjoiMmY1ZjI0MjBkN2FkYjY4MzkwY2M3NTU3M2E3NGVjMTU4ZDc5ZWYzOCIsIm4iOiJweXRob25sb2dnZXIiLCJpZCI6NTY4OTYxfQ==@logs-prod-us-central1.grafana.net/loki/api/v1/push",
-)
+   )
 import traceback
 
 try:
